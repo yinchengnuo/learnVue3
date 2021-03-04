5b0759855881bb1ebaeb34c0fcdc5f9ab1c30573
yinchengnuo <yinnuo96@163.com>
2021-03-04 21:47:30 +0800

    build

--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,17 @@ import Index from '../views/Index.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 export const routes = [
-  { name: 'Demo', path: '/Demo', component: () => import('../views/Demo.vue') },
+  { name: 'FuzzyQuery', path: '/FuzzyQuery', component: () => import('../views/FuzzyQuery.vue') },
   { path: '/', name: 'Index', component: Index },
   { name: 'Setup', path: '/Setup', component: () => import('../views/Setup.vue') },
   { name: 'Ref', path: '/Ref', component: () => import('../views/Ref.vue') },
   { name: 'Reactive', path: '/Reactive', component: () => import('../views/Reactive.vue') },
   { name: 'ToRef/s', path: '/ToRef/s', component: () => import('../views/toRef&s.vue') },
-  { name: 'Reactive', path: '/Reactive', component: () => import('../views/Reactive.vue') },
   { name: 'Computed', path: '/Computed', component: () => import('../views/Computed.vue') }
 ]
 
 export default createRouter({
   history: createWebHashHistory(),
-  routes
+  routes,
+  base: '/vue3'
 })
